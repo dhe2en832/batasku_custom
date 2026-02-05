98ef6e7c2af0f62b880d68b6844bde43787985ad
dhe2en832 <vespahitam65@gmail.com>
2026-02-05 09:29:00 +0700

    first commit - initial app structure

--- batasku_custom/hooks.py
+++ batasku_custom/hooks.py
@@ -247,3 +247,10 @@ app_license = "mit"
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+fixtures = [
+    {"dt": "Custom Field", "filters": [["module", "=", "Batasku Custom"]]},
+    {"dt": "Property Setter", "filters": [["module", "=", "Batasku Custom"]]},
+    {"dt": "Client Script", "filters": [["module", "=", "Batasku Custom"]]},
+    {"dt": "Server Script", "filters": [["module", "=", "Batasku Custom"]]}
+]
+
